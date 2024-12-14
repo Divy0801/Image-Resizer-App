78db4ba8e4f253bf92274a39a1c8a1d7be055ae6
Divyansh Gupta <divyanshpersonal08@gmail.com>
2024-12-14 23:31:03 +0530

    changed..

--- src/script.js
+++ src/script.js
@@ -4,7 +4,7 @@ const { BlobServiceClient } = require("@azure/storage-blob");
 const selectButton = document.getElementById("select-button");
 const fileInput = document.getElementById("file-input");
 
-const blobSasUrl = "https://images23f2.blob.core.windows.net/?sv=2022-11-02&ss=b&srt=co&sp=rwciytf&se=2024-12-30T01:11:14Z&st=2024-12-14T17:11:14Z&spr=https&sig=yBOswgjNBmKWPO6j3x2bcsslozdoa8bP%2FcZxzx6AIUg%3D";
+const blobSasUrl = "https://images23f2.blob.core.windows.net/?sv=2022-11-02&ss=b&srt=co&sp=rwiytf&se=2024-12-15T01:49:41Z&st=2024-12-14T17:49:41Z&spr=https&sig=PqYHeO2ysqHGIybNVttrzzxqzC6AvCfFWzOv0P%2BI6KY%3D"
 const blobServiceClient = new BlobServiceClient(blobSasUrl); 
 const containerName = "orgimages";
 const containerClient = blobServiceClient.getContainerClient(containerName);
